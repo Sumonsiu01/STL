0293a271c4380877f93cd4f37e52123f202f930c
Sumon Ahmed <85849219+Sumonsiu01@users.noreply.github.com>
2022-01-16 01:04:31 +0600

    Added more

---
+++ src/StirngToInt.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "123";
+    int a = stoi(s); // it will convert the string to integer
+    cout<<a<<endl;
+    a++;
+    cout<<a<<endl;//look here the integer increasing
+
+}
